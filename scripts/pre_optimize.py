1e68882b2c5db2c57bed820b980ae9458f8a52d9
YHordijk <42876712+YHordijk@users.noreply.github.com>
2022-01-19 20:00:22 +0100

    added termination checks

--- scripts/pre_optimize.py
+++ scripts/pre_optimize.py
@@ -19,9 +19,18 @@ def pre_optimize(mol, path):
     job = plams.AMSJob(molecule=plams.Molecule(mol.path), settings=settings, name=name + '_preopt')
     res = job.run()
 
-    if self.check_termination_succes(res):
+    if check_termination_succes(res):
         molecule = res.get_main_molecule()
         molecule.write(path)
 
         return molecule
     return mol
+
+
+def check_termination_succes(result :Results) -> bool:
+    term = result.readrkf('General', 'termination status', 'ams')
+    if term == 'NORMAL TERMINATION':
+        return True
+    elif 'NORMAL TERMINATION' in term:
+        return 'WARNING'
+    return False
