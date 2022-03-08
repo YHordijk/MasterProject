41573e51ddd03fbe27d53a3219c64d39752ae864
Yuman Hordijk <yumanhordijK@gmail.com>
2022-03-08 10:03:37 +0100

    added some kernels

--- scripts/regression/models.py
+++ scripts/regression/models.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import data_gen
 import sklearn.kernel_ridge
+import itertools
 
 
 def correlation(y, y_pred):
@@ -31,6 +32,31 @@ def ridge_regression(data, lam):
 	return {'a':a, 'R2':correlation(y, x@a)}
 
 
+
+class FeatureMap:
+	def __call__(self, x):
+		NotImplemented
+
+
+class LinearFM(FeatureMap):
+	def __call__(self, x):
+		return x
+
+class PolynomialFM(FeatureMap):
+	def __init__(self, n):
+		self.n = n
+
+	def __call__(self, x):
+		return np.array(list(itertools.combinations_with_replacement(x, self.n))).reshape(-1,1)
+
+
+class QuadraticFM(FeatureMap):
+	def __call__(self, x):
+		return np.array(list(itertools.combinations_with_replacement(x, 2))).reshape(-1,1)
+
+
+
+
 class KRR:
 	def __init__(self, kernel='polynomial', **kwargs):
 		self.kernel_type = kernel
@@ -38,28 +64,40 @@ class KRR:
 			self.hyperparameters = {'alpha':	kwargs.get('alpha',  1),
 									'degree': 	kwargs.get('degree', 1)}
 
-	def train(self, X, Y):
-		'''
-		X is an array where each row is a datapoint. The number of columns is te number of independent variables
-		Y is an array with observed outcomes corresponding to the data in X
-		'''
+	# def train(self, X, Y):
+	# 	'''
+	# 	X is an array where each row is a datapoint. The number of columns is te number of independent variables
+	# 	Y is an array with observed outcomes corresponding to the data in X
+	# 	'''
 
-		N = Y.size
-		m = X.shape[1]
+	# 	N = Y.size
+	# 	m = X.shape[1]
 
-	# def kernelize(self):
+	# def get_kernel(self):
 		
 
 
-help(sklearn.kernel_ridge)
+# help(sklearn.kernel_ridge)
 
-data = data_gen.random_data(600, 1, noise=0.1)
-# plt.scatter(data['x'][:,1:], data['y'])
-# for l in [1000,100,10,1, .1, .01, .001, .0001, .00001]:
-# 	a = ridge_regression(data,l)
-# 	plt.scatter(data['x'][:,1:], data['x']@a['a'])
+# data = data_gen.random_data(600, 1, noise=0.1)
+# # plt.scatter(data['x'][:,1:], data['y'])
+# # for l in [1000,100,10,1, .1, .01, .001, .0001, .00001]:
+# # 	a = ridge_regression(data,l)
+# # 	plt.scatter(data['x'][:,1:], data['x']@a['a'])
 
-# plt.show()
+# # plt.show()
 
-model = KRR()
-model.train(data['x'], data['y'])
+# model = KRR()
+# model.train(data['x'], data['y'])
+
+
+U = [1, 2, 3, 4]
+V = [5, 6, 7, 8]
+
+fm = PolynomialFM(5)
+
+fu = fm(U)
+fv = fm(V)
+
+print(fu, fv)
+print(fu.T @ fv)
